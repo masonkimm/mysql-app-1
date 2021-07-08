34b8d0fe3acd92c7820dd2b05b2f9d52426e177a
SungJin Kim <sungjinkimm@gmail.com>
2021-07-08 10:50:59 -0400

    mysql db connected

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `employeeDB`.`employees` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `age` INT NOT NULL,
+  `country` VARCHAR(45) NOT NULL,
+  `position` VARCHAR(45) NOT NULL,
+  `salary` INT NOT NULL,
+  PRIMARY KEY (`id`));
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+//db
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'password',
+  database: 'employeeDB',
+});
+
+app.post('/create', (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const salary = req.body.salary;
+
+  db.query(
+    'INSERT INTO employees (name, age, country, position, salary) VALUES (?, ? , ? ,? ,?)',
+    [name, age, country, position, salary],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send('Values inserted');
+      }
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Live on PORT: ${PORT}`);
+});
